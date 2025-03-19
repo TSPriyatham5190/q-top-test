a5163ea35ce228e40b79c921cf97e8288ee578ec
TSPriyatham5190 <151216194+TSPriyatham5190@users.noreply.github.com>
2025-03-19 07:03:23 +0000

    names

--- app.py
+++ app.py
@@ -1,14 +1,39 @@
 from flask import Flask
+import os
+import datetime
+import subprocess
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<div>hihhihi</div>"
 
-@app.route("/htop")
-def hello_htop():
-    return "<p>Hello, World!</p>"
 
-if __name__ == "__main__":
-    app.run()
+
+@app.route('/htop')
+def htop():
+    
+    username = os.getenv("USER") or os.getenv("USERNAME")
+
+    
+    ist_time = datetime.datetime.utcnow() + datetime.timedelta(hours=5, minutes=30)
+    formatted_time = ist_time.strftime("%Y-%m-%d %H:%M:%S.%f")
+
+   
+    top_output = subprocess.getoutput("top -b -n 1")
+
+    return f"""
+    <pre>
+    Name: Priya
+    Username: {username}
+    Server Time (IST): {formatted_time}
+
+    TOP output:
+    {top_output}
+    </pre>
+    """
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
