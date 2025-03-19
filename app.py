0f00c3f24220263475e47faa6b90c087ca968c84
TSPriyatham5190 <151216194+TSPriyatham5190@users.noreply.github.com>
2025-03-19 06:49:56 +0000

    htop

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
+@app.route("/htop")
+def hello_htop():
+    return "<p>Hello, World!</p>"
 
 if __name__ == "__main__":
     app.run()
